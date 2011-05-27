46f36bbed747a4943e12d453576923b05ffd2b79
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2011-05-27 19:11:44 +0200

    * ruby/damage/types.rb: v0 for internal structures

--- ruby/damage/types.rb
+++ ruby/damage/types.rb
@@ -5,10 +5,12 @@ module Damage
 
       # Data used only for SORT fields
       attr_accessor :sort_field, :sort_key
-      def initialize(tree)
-        @name = tree["name"]
-        @default_val = tree["default"]
-        case tree["quantity"]
+      def initialize(field)
+        @name = field["name"]
+        @default_val = field["default"]
+        @attribute = :none
+        @qty = :single
+        case field["quantity"]
         when "SINGLE", nil
           @qty = :single
         when "LIST"
@@ -16,20 +18,22 @@ module Damage
         when "ARRAY"
           @qty = :array
         end
-        case tree["attribute"]
+        case field["attribute"]
         when "META"
           @attribute = :meta
         when "SORT"
           @attribute = :sort
-          @sort_field = tree["sort_field"]
-          @sort_key = tree["sort_key"]
+          @sort_field = field["sort_field"]
+          @sort_key = field["sort_key"]
         when "PASS"
           @attribute = :pass
+        when "CONTAINER"
+          @attribute = :container
         else
-          @attribute = :none
+          raise("Unknown field attribute #{field["attribute"]}")
         end
 
-        case tree["type"]
+        case field["type"]
         when "String"
           @data_type = "char*"
           @category = :simple
@@ -41,15 +45,53 @@ module Damage
         when /EXT\(([\w+ ]*)\)/
           @data_type = $1
           @category = :simple
-        when /STRUCT\(([\w+ ]*)\)/
-          @data_type = $1
+        when /(S|STRUCT)\(([\w+ ]*)\)/
+          @data_type = $2
           @category = :intern
+        else
+          raise("Field #{@name} has no data type...")
         end
         
       end
     end
 
-    class Struct
+    class Entry
+      attr_accessor :name, :attribute, :fields
+      def initialize(entry)
+        @name = entry["name"]
+        @attribute = :none
+        @cleanup = nil
+        @fields = []
+        case entry["attribute"]
+        when "TOP"
+          @attribute = :top
+        else
+          raise("Unknown entry attribute #{entry["attribute"]}")
+        end
+        entry["fields"].each() { |field|
+          @fields << Field.new(field)
+        }
+      end
+    end
+    class Config
+      attr_accessor :libname
+      def initialize(config)
+        @libname = config["libname"]
+      end
+    end
+    class Descriptrion
+      attr_accessor :config
+      attr_accessor :entries, :top_entry
+
+      def initialize(tree)
+        @config = Config.new(tree["config"])
+        @entries = []
+        tree["entries"].each() { |entry|
+          _entry = Entry.new(entry)
+          @top_entry = _entry if (_entry.attribute == :top)
+          @entries << _entry
+        }
+      end
     end
   end
 end
