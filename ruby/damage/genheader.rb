390d0ed32cc358fd5047d31c8dd996b5ac1aee32
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2011-05-31 10:14:43 +0200

    * ruby/damage/genheader.rb: Add Common.c generation

--- ruby/damage/genheader.rb
+++ ruby/damage/genheader.rb
@@ -2,12 +2,15 @@ module Damage
   module GenHeader
 
     def write(description)
-      outputH = Damage::Files.createAndOpen("gen/#{description.config.libname}/include/", "#{description.config.libname}.h")
-      self.genHeader(outputH, description)
-      outputH.close()
-      outputH = Damage::Files.createAndOpen("gen/#{description.config.libname}/include/#{description.config.libname}/", "common.h")
-      self.genCommonH(outputH, description)
-      outputH.close()
+      output = Damage::Files.createAndOpen("gen/#{description.config.libname}/include/", "#{description.config.libname}.h")
+      self.genHeader(output, description)
+      output.close()
+      output = Damage::Files.createAndOpen("gen/#{description.config.libname}/include/#{description.config.libname}/", "common.h")
+      self.genCommonH(output, description)
+      output.close()
+      output = Damage::Files.createAndOpen("gen/#{description.config.libname}/src/", "common.c")
+      self.genCommonC(output, description)
+      output.close()
     end
     module_function :write
 
@@ -54,5 +57,199 @@ extern int __#{libName}_line;
 "
     end
     module_function :genCommonH
+
+    def genCommonC(output, description)
+      libName = description.config.libname
+output.puts "
+#include <assert.h>
+#include <errno.h>
+#include <setjmp.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <libxml/xmlreader.h>
+#include \"#{libName}.h\"
+#include \"#{libName}/common.h\"
+
+void *__#{libName}_malloc(unsigned long size)
+{
+	void *ptr = malloc(size);
+	if (ptr == NULL) {
+		__#{libName}_error(\"Failed to allocate memory: %s\\n\", errno,
+				strerror(errno));
+	}
+	return ptr;
+}
+
+void *__#{libName}_realloc(void *ptr, unsigned long size)
+{
+	void *nptr = realloc(ptr, size);
+	if (nptr == NULL) {
+		__#{libName}_error(\"Failed to reallocate memory: %s\\n\", errno,
+				strerror(errno));
+	}
+	return nptr;
+}
+
+void __#{libName}_free(void *ptr)
+{
+	if (ptr != NULL) {
+		free(ptr);
+		ptr = NULL;
+	}
+}
+
+
+/*************************************
+ * HELPER FUNCTIONS
+ *************************************/
+
+/**
+ * Parsing helper. Compare the node name to expected values.
+ * If an expected node is found, (open element), we eat the next node, because
+ * the XML parser returns a weird token after each open elemene,t before the value.
+ * matches contains an array of expected string. The last entry must be NULL so we now how 
+ * many elements there are.
+ * @param[in] reader XML Reader
+ * @param[in] name Name of the current node
+ * @param[in] matches Array of expected string
+ * @return ID of the matched string
+ * @retval < number of strings Match
+ * @retval >= number of string No match found.
+ */
+int __#{libName}_compare(const char *name, const char *matches[])
+{
+	int i = 0;
+	for (i = 0; matches[i] != NULL; i++) {
+		if (!strcmp(name, matches[i])) {
+			return i;
+		}
+	}
+	return i;
+}
+
+/**
+ * Get the value of the current XML node.
+ * This is equivalent to #__#{libName}_get_value but the string is duplicate
+ * so it won't change when new reads are dont to the XML file.
+ * @param[in] reader XML Reader
+ * @return Value of the current node 
+ */
+char *__#{libName}_read_value_str(xmlNodePtr node)
+{
+	char *val = NULL;
+	if (node->children && node->children->content)
+		val = strdup((char *)node->children->content);
+	return val;
+}
+
+/**
+ * Get the value of the current XML node as an unsigned long
+ * @param[in] reader XML Reader
+ * @return Value of the current node
+ */
+unsigned long __#{libName}_read_value_ulong(xmlNodePtr node)
+{
+	unsigned long val;
+	val = strtoul((char *)node->children->content, NULL, 10);
+	return val;
+}
+
+/**
+ * Get the value of the current XML node as a double
+ * @param[in] reader XML Reader
+ * @return Value of the current node
+ */
+double __#{libName}_read_value_double(xmlNodePtr node)
+{
+	double val;
+	val = strtod((char *)node->children->content, NULL);
+	return val;
+}
+
+/**
+ * Get the value of the current XML node.
+ * This is equivalent to #__#{libName}_get_value but the string is duplicate
+ * so it won't change when new reads are dont to the XML file.
+ * @param[in] reader XML Reader
+ * @return Value of the current node 
+ */
+char *__#{libName}_read_value_str_attr(xmlAttrPtr node)
+{
+	char *val = NULL;
+	if (node->children && node->children->content)
+		val = strdup((char *)node->children->content);
+	return val;
+}
+
+/**
+ * Get the value of the current XML node as an unsigned long
+ * @param[in] reader XML Reader
+ * @return Value of the current node
+ */
+unsigned long __#{libName}_read_value_ulong_attr(xmlAttrPtr node)
+{
+	unsigned long val;
+	val = strtoul((char *)node->children->content, NULL, 10);
+	return val;
+}
+
+/**
+ * Get the value of the current XML node as a double
+ * @param[in] reader XML Reader
+ * @return Value of the current node
+ */
+double __#{libName}_read_value_double_attr(xmlAttrPtr node)
+{
+	double val;
+	val = strtod((char *)node->children->content, NULL);
+	return val;
+}
+
+static struct flock lock;
+static FILE* __#{libName}_filelock = NULL;
+
+
+int __#{libName}_acquire_flock(const char* filename){
+	char* lock_file;
+	
+	if(__#{libName}_filelock != NULL){
+		/* We already have the lock */
+		return 0;
+	}
+	lock_file = malloc(strlen(filename) + 10);
+	sprintf(lock_file, \"%s.lock\", filename);
+	lock.l_whence = SEEK_SET;
+	lock.l_start = 0;
+	lock.l_len = 0;
+	lock.l_pid = getpid();
+	lock.l_type = F_WRLCK;
+	__#{libName}_filelock = fopen(lock_file, \"w+\");
+	free(lock_file);
+	if(__#{libName}_filelock == NULL){
+		return 1;
+	}
+	while(fcntl(fileno(__#{libName}_filelock), F_SETLKW, &lock))
+		if(errno != EINTR)
+			return 1;
+
+	return 0;
+}
+
+int __#{libName}_release_flock(){
+	lock.l_type = F_UNLCK;
+	while(fcntl(fileno(__#{libName}_filelock), F_SETLKW, &lock))
+		if(errno != EINTR)
+			return 1;
+	fclose(__#{libName}_filelock);
+	__#{libName}_filelock = NULL;
+	return 0;
+}
+
+"
+    end
+    module_function :genCommonC
   end
 end
