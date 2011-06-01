5e622f7ec78d9538c4f635572543090348579a5c
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2011-06-01 10:31:14 +0200

    * ruby/damage/c/tests.rb: Add tests for binary format

--- ruby/damage/c/tests.rb
+++ ruby/damage/c/tests.rb
@@ -19,24 +19,17 @@ module Damage
     module Tests
 
       def write(description)
-        output = Damage::Files.createAndOpen("gen/#{description.config.libname}/test/", "create_dump_and_reload.c")
-        self.genTest1(output, description)
-        output.close()
+        self.genTest1(description)
+        self.genTest2(description)
       end
       module_function :write
 
+      
 
       private
-      def genTest1(output, description)  
-        libName = description.config.libname
-        output.puts "
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <#{libName}.h>
-#include <string.h>
 
-"
+      def genDBCreator(output, description)
+        libName = description.config.libname
         description.entries.each() { |name, entry|
           output.puts "__#{libName}_#{entry.name}* create#{entry.name}(int first);"
         }
@@ -58,6 +51,21 @@ module Damage
 \treturn ptr;
 }"
         }
+      end
+      module_function :genDBCreator
+
+      def genTest1(description)  
+        libName = description.config.libname
+        output = Damage::Files.createAndOpen("gen/#{libName}/test/", "create_dump_and_reload.c")
+        output.puts "
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <#{libName}.h>
+#include <string.h>
+
+"
+        genDBCreator(output, description)
         output.puts "
 int main(int argc, char *argv[])
 {
@@ -82,8 +90,49 @@ int main(int argc, char *argv[])
 }
 
 "
+        output.close()
+
       end
       module_function :genTest1
+
+      def genTest2(description)  
+        libName = description.config.libname
+         output = Damage::Files.createAndOpen("gen/#{libName}/test/", "create_dump_and_reload_binary.c")
+       output.puts "
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <#{libName}.h>
+#include <string.h>
+
+"
+        genDBCreator(output, description)
+        output.puts "
+int main(int argc, char *argv[])
+{
+	__#{libName}_#{description.top_entry.name} *ptr = create#{description.top_entry.name}(1);
+	if (argc < 2) {
+		fprintf(stderr, \"Usage: %s file\\n\", argv[0]);
+		exit(1);
+	}
+	if (__#{libName}_#{description.top_entry.name}_binary_dump_file(argv[1], ptr) == 0) {
+		fprintf(stderr, \"Failed writing to %s\\n\", argv[1]);
+		exit(2);
+	}
+	__#{libName}_#{description.top_entry.name}_free(ptr);
+
+/*	ptr = __#{libName}_#{description.top_entry.name}_xml_parse_file(argv[1]);
+	if (ptr == NULL) {
+		fprintf(stderr, \"Failed to parse %s\\n\", argv[1]);
+		exit(3);
+    }
+	__#{libName}_#{description.top_entry.name}_free(ptr); */
+	return 0;
+}
+
+"
+      end
+      module_function :genTest2
     end
   end
 end
