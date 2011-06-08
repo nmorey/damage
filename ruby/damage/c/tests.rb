8fa3b5f34089d21dd86bac87bd4365c4bf90b760
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2011-06-08 16:29:16 +0200

    * ruby/damage/c/tests.rb: Reindent + add betetr DB creator and verbose test1

--- ruby/damage/c/tests.rb
+++ ruby/damage/c/tests.rb
@@ -15,115 +15,144 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 module Damage
-  module C
-    module Tests
+    module C
+        module Tests
 
-      def write(description)
-        libName = description.config.libname
-        output = Damage::Files.createAndOpen("gen/#{libName}/.db/", "dummy")
-        self.genTest1(description)
-        self.genTest2(description)
-        self.genTest3(description)
-        self.genTest4(description)
-      end
-      module_function :write
-
-      
-
-      private
-
-      def genDBCreator(output, description, nb_neighbours=1)
-        libName = description.config.libname
-        description.entries.each() { |name, entry|
-          output.puts "__#{libName}_#{entry.name}* create#{entry.name}(int first);"
-        }
-
-        description.entries.each() { |name, entry|
-          output.puts "__#{libName}_#{entry.name}* create#{entry.name}(int first __attribute__((unused))){
-\t__#{libName}_#{entry.name}* ptr = NULL;"
-          if entry.attribute == :listable
-            output.puts "\t__#{libName}_#{entry.name}* prev = NULL, *pfirst = NULL;" 
-            output.puts "\twhile(first >=0){" 
-          end
-          output.puts "
-\tptr = __#{libName}_#{entry.name}_alloc();"
-          entry.fields.each() { |field|
-            if field.target != :mem && field.category == :intern then
-              output.puts "\tptr->#{field.name} = create#{field.data_type}(#{nb_neighbours});"
+            def write(description)
+                libName = description.config.libname
+                output = Damage::Files.createAndOpen("gen/#{libName}/.db/", "dummy")
+                self.genTest1(description)
+                self.genTest2(description)
+                self.genTest3(description)
+                self.genTest4(description)
             end
-          }
-          if entry.attribute == :listable then
-            output.puts "\tfirst--;"
-            output.puts "\tif(prev){"
-            output.puts "\t\tprev->next = ptr;"
-            output.puts "\t} else {"
-            output.puts "\t\tpfirst = ptr;"
-            output.puts "\t}"
-            output.puts "\tprev = ptr;"
-            output.puts "\t}"
-            output.puts "\treturn pfirst;\n}\n"
-          else
+            module_function :write
 
-            output.puts "
+            
+
+            private
+
+            def genDBCreator(output, description, nb_neighbours=1)
+                libName = description.config.libname
+                description.entries.each() { |name, entry|
+                    output.puts "__#{libName}_#{entry.name}* create#{entry.name}(int first);"
+                }
+
+                description.entries.each() { |name, entry|
+                    output.puts "__#{libName}_#{entry.name}* create#{entry.name}(int first __attribute__((unused))){
+\t__#{libName}_#{entry.name}* ptr = NULL;"
+                    if entry.attribute == :listable
+                        output.puts "\t__#{libName}_#{entry.name}* prev = NULL, *pfirst = NULL;" 
+                        output.puts "\twhile(first >=0){" 
+                    end
+                    output.puts "
+\tptr = __#{libName}_#{entry.name}_alloc();"
+                    entry.fields.each() { |field|
+                        next if field.target != :both
+                        case field.category
+                        when :intern
+                            output.puts "\tptr->#{field.name} = create#{field.data_type}(#{nb_neighbours});"
+                        when :simple
+                            next if field.qty != :single
+                            case field.data_type
+                            when "char*"
+                                output.puts "\t{\n"
+                                output.puts "\t\tunsigned long i, len = rand()%128;\n"
+                                output.puts "\t\tchar _str[129];\n"
+                                output.puts "\t\tfor(i=0; i < len; i++){ _str[i] = (rand() % 26) + 'a';}\n"
+                                output.puts "\t\t_str[len] = 0;\n"
+                                output.puts "\t\tptr->#{field.name} = strdup(_str);\n"
+                                output.puts "\t}\n"
+                            when "unsigned long"
+                                output.puts "\tptr->#{field.name} = 0;"
+                            when "double"
+                                output.puts "\tptr->#{field.name} = drand48();"
+                            end
+                        when :id, :idref
+                            output.puts "\t{\n"
+                            output.puts "\t\tchar _str[1024];;\n"
+                            if field.category == :id then
+                                output.puts "\t\tsnprintf(_str, 1024, \"#{entry.name}-%d\", first);\n"
+                            else
+                                output.puts "\t\tsnprintf(_str, 1024, \"#{field.data_type}-%d\", first);\n"
+                            end
+                            output.puts "\t\tptr->#{field.name}_str = strdup(_str);\n"
+                            output.puts "\t\tptr->#{field.name} = first;\n"
+                            output.puts "\t}\n"
+                        end
+                    }
+                    if entry.attribute == :listable then
+                        output.puts "\tfirst--;"
+                        output.puts "\tif(prev){"
+                        output.puts "\t\tprev->next = ptr;"
+                        output.puts "\t} else {"
+                        output.puts "\t\tpfirst = ptr;"
+                        output.puts "\t}"
+                        output.puts "\tprev = ptr;"
+                        output.puts "\t}"
+                        output.puts "\treturn pfirst;\n}\n"
+                    else
+
+                        output.puts "
 \treturn ptr;
 }"
-          end
-        }
-      end
-      module_function :genDBCreator
+                    end
+                }
+            end
+            module_function :genDBCreator
 
 
-      def genDBModifyer(output, description, rowip = false)
-        libName = description.config.libname
-        funcPrefix=""
-        funcPrefix="rowip_" if rowip != false
-        description.entries.each() { |name, entry|
-          output.puts "void #{funcPrefix}modify#{entry.name}(__#{libName}_#{entry.name}* ptr);"
-        }
+            def genDBModifyer(output, description, rowip = false)
+                libName = description.config.libname
+                funcPrefix=""
+                funcPrefix="rowip_" if rowip != false
+                description.entries.each() { |name, entry|
+                    output.puts "void #{funcPrefix}modify#{entry.name}(__#{libName}_#{entry.name}* ptr);"
+                }
 
-        description.entries.each() { |name, entry|
-          output.puts "void #{funcPrefix}modify#{entry.name}(__#{libName}_#{entry.name}* ptr){
+                description.entries.each() { |name, entry|
+                    output.puts "void #{funcPrefix}modify#{entry.name}(__#{libName}_#{entry.name}* ptr){
 \t__#{libName}_#{entry.name}* el __attribute__ ((unused)) = NULL;"
-          if entry.attribute == :listable
-            if rowip != false
-              output.puts "\tfor(el = ptr; el != NULL; el = __#{libName.upcase}_ROWIP_PTR(el, next)){" 
-            else
-              output.puts "\tfor(el = ptr; el != NULL; el = el->next){"
+                    if entry.attribute == :listable
+                        if rowip != false
+                            output.puts "\tfor(el = ptr; el != NULL; el = __#{libName.upcase}_ROWIP_PTR(el, next)){" 
+                        else
+                            output.puts "\tfor(el = ptr; el != NULL; el = el->next){"
+                        end
+                    else
+                        output.puts "\tel = ptr;"
+                    end
+                    entry.fields.each() { |field|
+                        if field.target != :mem then
+                            if field.category == :intern then
+                                output.puts "\tif(el->#{field.name} != NULL)"
+                                if rowip != false
+                                    output.puts "\t\t#{funcPrefix}modify#{field.data_type}(__#{libName.upcase}_ROWIP_PTR(el, #{field.name}));"
+                                else
+                                    output.puts "\t\t#{funcPrefix}modify#{field.data_type}(el->#{field.name});"
+                                end
+                            elsif field.data_type == "unsigned long"
+                                output.puts "\tel->#{field.name} = 42;"
+                                #                output.printf("\printf(\"#{funcPrefix}Changing %s.%s to 42\\n\");\n", entry.name, field.name)
+                            elsif field.data_type == "double"
+                                output.puts "\tel->#{field.name} = 42.0;"
+                            end
+                        end
+                    }
+                    if entry.attribute == :listable then
+                        output.puts "\t}"
+                    end
+
+                    output.puts "}"
+
+                }
             end
-          else
-            output.puts "\tel = ptr;"
-          end
-          entry.fields.each() { |field|
-            if field.target != :mem then
-              if field.category == :intern then
-                output.puts "\tif(el->#{field.name} != NULL)"
-                if rowip != false
-                  output.puts "\t\t#{funcPrefix}modify#{field.data_type}(__#{libName.upcase}_ROWIP_PTR(el, #{field.name}));"
-                else
-                  output.puts "\t\t#{funcPrefix}modify#{field.data_type}(el->#{field.name});"
-                end
-              elsif field.data_type == "unsigned long"
-                output.puts "\tel->#{field.name} = 42;"
-#                output.printf("\printf(\"#{funcPrefix}Changing %s.%s to 42\\n\");\n", entry.name, field.name)
-              elsif field.data_type == "double"
-                output.puts "\tel->#{field.name} = 42.0;"
-              end
-            end
-          }
-          if entry.attribute == :listable then
-            output.puts "\t}"
-          end
+            module_function :genDBModifyer
 
-          output.puts "}"
-
-        }
-      end
-      module_function :genDBModifyer
-
-      def genTest1(description)  
-        libName = description.config.libname
-        output = Damage::Files.createAndOpen("gen/#{libName}/test/", "create_dump_and_reload.c")
-        output.puts "
+            def genTest1(description)  
+                libName = description.config.libname
+                output = Damage::Files.createAndOpen("gen/#{libName}/test/", "create_dump_and_reload.c")
+                output.puts "
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -131,8 +160,8 @@ module Damage
 #include <string.h>
 
 "
-        genDBCreator(output, description)
-        output.puts "
+                genDBCreator(output, description)
+                output.puts "
 int main()
 {
    char* file=\".db/test1.xml\";
@@ -142,32 +171,35 @@ int main()
 		fprintf(stderr, \"Failed writing to %s\\n\", file);
 		exit(2);
 	}
+    printf(\"Generated DB\\n\");
 	if (__#{libName}_#{description.top_entry.name}_xml_dump_file(file, ptr, 1) < 0) {
 		fprintf(stderr, \"Failed writing to %s in gzipped mode\\n\", file);
 		exit(3);
 	}
-
+    printf(\"Wrote XML DB\\n\");
 	__#{libName}_#{description.top_entry.name}_free(ptr);
-
+    printf(\"Freed DB\\n\");
 	ptr = __#{libName}_#{description.top_entry.name}_xml_parse_file(file);
 	if (ptr == NULL) {
 		fprintf(stderr, \"Failed to parse %s\\n\", file);
 		exit(4);
     }
+    printf(\"Parsed XML DB\\n\");
 	__#{libName}_#{description.top_entry.name}_free(ptr);
+    printf(\"Freed DB\\n\");
 	return 0;
 }
 
 "
-        output.close()
+                output.close()
 
-      end
-      module_function :genTest1
+            end
+            module_function :genTest1
 
-      def genTest2(description)  
-        libName = description.config.libname
-        output = Damage::Files.createAndOpen("gen/#{libName}/test/", "create_dump_and_reload_binary.c")
-        output.puts "
+            def genTest2(description)  
+                libName = description.config.libname
+                output = Damage::Files.createAndOpen("gen/#{libName}/test/", "create_dump_and_reload_binary.c")
+                output.puts "
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -175,8 +207,8 @@ int main()
 #include <string.h>
 
 "
-        genDBCreator(output, description, 2)
-        output.puts "
+                genDBCreator(output, description, 2)
+                output.puts "
 int main()
 {
    char* file=\".db/test2.db\";
@@ -210,13 +242,13 @@ int main()
 }
 
 "
-      end
-      module_function :genTest2
+            end
+            module_function :genTest2
 
-      def genTest3(description)  
-        libName = description.config.libname
-        output = Damage::Files.createAndOpen("gen/#{libName}/test/", "create_dump_and_reload_binary_long.c")
-        output.puts "
+            def genTest3(description)  
+                libName = description.config.libname
+                output = Damage::Files.createAndOpen("gen/#{libName}/test/", "create_dump_and_reload_binary_long.c")
+                output.puts "
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -224,8 +256,8 @@ int main()
 #include <string.h>
 
 "
-        genDBCreator(output, description, 15)
-        output.puts "
+                genDBCreator(output, description, 15)
+                output.puts "
 int main()
 {
    char* file=\".db/test3.db\";
@@ -261,14 +293,14 @@ int main()
 }
 
 "
-        end
-        module_function :genTest3
+            end
+            module_function :genTest3
 
-        # Test ROWIP feature
-        def genTest4(description)  
-          libName = description.config.libname
-          output = Damage::Files.createAndOpen("gen/#{libName}/test/", "create_dump_and_reload_rowip.c")
-          output.puts "
+            # Test ROWIP feature
+            def genTest4(description)  
+                libName = description.config.libname
+                output = Damage::Files.createAndOpen("gen/#{libName}/test/", "create_dump_and_reload_rowip.c")
+                output.puts "
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -276,10 +308,10 @@ int main()
 #include <string.h>
 
 "
-          genDBCreator(output, description, 10)
-          genDBModifyer(output, description)
-          genDBModifyer(output, description, 1)
-          output.puts "
+                genDBCreator(output, description, 10)
+                genDBModifyer(output, description)
+                genDBModifyer(output, description, 1)
+                output.puts "
 
 int main()
 {
@@ -333,8 +365,8 @@ int main()
 }
 
 "
+            end
+            module_function :genTest4
         end
-        module_function :genTest4
-      end
     end
-  end
+end
