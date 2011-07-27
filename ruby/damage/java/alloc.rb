f7c879967ffe3a9cf021b4edd9f971c9c5d95ce7
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2011-07-27 09:57:13 +0200

    * ruby/damage/java/alloc.rb: Forgot alloc file
    Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

---
+++ ruby/damage/java/alloc.rb
@@ -0,0 +1,46 @@
+# Copyright (C) 2011  Nicolas Morey-Chaisemartin <nicolas@morey-chaisemartin.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+module Damage
+    module Java
+        module Alloc
+            
+            def write(output, libName, entry, pahole, params)
+                output.printf("\t/** Default constructor */")
+                output.printf("\tpublic #{params[:class]}(){\n")
+
+                entry.fields.each() { |field|
+                    case field.attribute
+                    when :sort
+                        next #FIXME
+                        output.printf("\t\tptr->s_%s = NULL;\n", field.name)
+                        output.printf("\t\tptr->n_%s = 0UL;\n", field.name)
+                    else
+                        output.printf("\t\t%s = %s;\n", field.name, field.java_default_val)
+                    end
+                    if ((field.qty == :list) && (field.category == :simple || field.category == :enum))
+                        output.printf("\t\tptr->%sLen = 0;\n", field.name) 
+                    end
+                }
+
+                output.printf("\t\tnext = null;\n")         if entry.attribute == :listable
+                output.printf("\t}\n\n")
+            end
+
+            module_function :write
+        end
+    end
+end
