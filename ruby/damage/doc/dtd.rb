af83d964eb903873ed812adf969dfa3ab09c54fa
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2012-03-27 17:51:54 +0200

    * ruby/damage/doc/dtd.rb: Fix DTD generation
    Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

--- ruby/damage/doc/dtd.rb
+++ ruby/damage/doc/dtd.rb
@@ -15,83 +15,84 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 module Damage
-  module Doc
-    module DTD
+    module Doc
+        module DTD
 
 
-      def write(description)
-        output = Damage::Files.createAndOpen("gen/#{description.config.libname}/doc/", "#{description.config.libname}.dtd")
-        genDTD(output, description)
-        output.close()
-      end
-      module_function :write
-
-      private
-      def genDTD(output, description)
-
-        containers={}
-
-        description.entries.each() {|name, entry|
-
-          strList=[]
-          output.printf("<!ELEMENT #{entry.name} ");
-          if entry.children.length > 0 then
-            output.printf("(");
-            comma=""
-            entry.fields.each() {|field|
-              case field.qty
-              when :single,:container
-                maxOccurs = "?"
-                maxOccurs = "" if field.required == true
-              when :list
-                maxOccurs = "*"
-                maxOccurs = "+" if field.required == true
-              end
-              if ( field.target != :mem && field.is_attribute != true) then
-                if field.attribute == :container || field.category == :simple then
-                  output.printf("#{comma}#{field.name}#{maxOccurs}")
-                  if field.category == :simple
-                    strList << "<!ELEMENT #{field.name} (#PCDATA)>\n"
-                  end
-                else
-                  output.printf("#{comma}#{field.data_type}#{maxOccurs}")
-                end
-                comma=", "
-              end
-            } 
-            output.printf(")");
-          else
-            output.printf("EMPTY");
-          end
-          output.printf(">\n", entry.name);
-
-
-          entry.fields.each() {|field|
-            required= "#IMPLIED"
-            required= "#REQUIRED" if field.required == true
-            xmlType = "CDATA"
-            xmlType = field.enumList if field.enumList != nil
-            xmlType = "ID" if (field.category == :id)
-            xmlType = "IDREF" if (field.category == :idref)
-            if ( field.target != :mem && field.is_attribute == true) then
-              output.printf("<!ATTLIST #{entry.name} #{field.name} #{xmlType} #{required}>\n")
+            def write(description)
+                output = Damage::Files.createAndOpen("gen/#{description.config.libname}/doc/", "#{description.config.libname}.dtd")
+                genDTD(output, description)
+                output.close()
             end
-          }
-          if entry.attribute == :top
-            output.printf("<!ATTLIST #{entry.name} xsi:noNamespaceSchemaLocation CDATA #IMPLIED>\n");
-            output.printf("<!ATTLIST #{entry.name} xmlns:xsi CDATA #IMPLIED>\n");
-          end
-          strList.each() { |strEntry|
-            output.printf(strEntry)
-          }
-          output.printf("\n");
-        }
-        description.containers.each() { |name, type|
-          output.printf("<!ELEMENT #{name} (#{type}*)>\n");
-        }
+            module_function :write
 
-      end
-      module_function :genDTD
+            private
+            def genDTD(output, description)
+
+                containers={}
+                simpleList={}
+                description.entries.each() {|name, entry|
+
+                    strList=[]
+                    output.printf("<!ELEMENT #{entry.name} ");
+                    if entry.children.length > 0 then
+                        output.printf("(");
+                        comma=""
+                        entry.fields.each() {|field|
+                            case field.qty
+                            when :single,:container
+                                maxOccurs = "?"
+                                maxOccurs = "" if field.required == true
+                            when :list
+                                maxOccurs = "*"
+                                maxOccurs = "+" if field.required == true
+                            end
+                            if ( field.target != :mem && field.is_attribute != true) then
+                                if field.attribute == :container || field.category == :simple then
+                                    output.printf("#{comma}#{field.name}#{maxOccurs}")
+                                    if field.category == :simple && simpleList[field.name] == nil
+                                        strList << "<!ELEMENT #{field.name} (#PCDATA)>\n"
+                                    end
+                                    simpleList[field.name] = true
+                                else
+                                    output.printf("#{comma}#{field.data_type}#{maxOccurs}")
+                                end
+                                comma=", "
+                            end
+                        } 
+                        output.printf(")");
+                    else
+                        output.printf("EMPTY");
+                    end
+                    output.printf(">\n", entry.name);
+
+
+                    entry.fields.each() {|field|
+                        required= "#IMPLIED"
+                        required= "#REQUIRED" if field.required == true
+                        xmlType = "CDATA"
+                        xmlType = field.enumList if field.enumList != nil
+                        xmlType = "ID" if (field.category == :id)
+                        xmlType = "IDREF" if (field.category == :idref)
+                        if ( field.target != :mem && field.is_attribute == true) then
+                            output.printf("<!ATTLIST #{entry.name} #{field.name} #{xmlType} #{required}>\n")
+                        end
+                    }
+                    if entry.attribute == :top
+                        output.printf("<!ATTLIST #{entry.name} xsi:noNamespaceSchemaLocation CDATA #IMPLIED>\n");
+                        output.printf("<!ATTLIST #{entry.name} xmlns:xsi CDATA #IMPLIED>\n");
+                    end
+                    strList.each() { |strEntry|
+                        output.printf(strEntry)
+                    }
+                    output.printf("\n");
+                }
+                description.containers.each() { |name, type|
+                    output.printf("<!ELEMENT #{name} (#{type}*)>\n");
+                }
+
+            end
+            module_function :genDTD
+        end
     end
-  end
 end
