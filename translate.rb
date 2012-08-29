cf8d414bf29d779b970af28cb4fb863629a7429b
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2012-08-29 11:14:06 +0200

    * translate.rb: Support multiple YAML files
    Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>

--- translate.rb
+++ translate.rb
@@ -19,18 +19,60 @@ $LOAD_PATH.push(File.dirname(__FILE__) + "/ruby/")
 
 require 'yaml'
 require 'damage'
+require 'optparse'
 
-f = File.open(ARGV[0])
-tree = YAML::load(f)
+def mergeYAML(tree1, tree2)
+    raise("Cannot merge this sections (Hash vs Array)") if tree1.class != tree2.class
+    if(tree1.class == Hash) then
+        tree2.each(){|name, subsubtree|
+            if(tree1[name] == nil)
+                tree1[name] = tree2[name]
+            else
+                mergeYAML(tree1[name], tree2[name])
+            end
+        }
+    elsif(tree1.class == Array)
+        tree2.each(){|el|
+            tree1 << el
+        }
+    else
+        raise("Unsupported subtree to merge")
+    end
+end
+
+opts={}
+opts[:file] = []
+optsParser = OptionParser.new(nil, 60)
+optsParser.banner = "Usage: #{File.basename(__FILE__)} [OPTIONS...] --file file.yaml [--file file2.yaml...]"
+optsParser.separator ""
+optsParser.separator "Options:"
+
+optsParser.on("-f", "--file <file.yaml>", String, "YAML DB description.") {|val| opts[:file] << val}
+optsParser.on("-p", "--pahole <pahole.result>", String, "Path to pahole size infos.") {|val| opts[:pahole] = val}
+optsParser.on("-h", "--help",  "Display usage.") { |val| puts optsParser.to_s; exit 0 }
+rest = optsParser.parse(ARGV);
+
+raise("Required YAML files")  if opts[:file].length == 0
+tree = nil
+opts[:file].each(){|fName|
+    f = File.open(fName)
+    subtree = YAML::load(f)
+    if tree != nil
+        mergeYAML(tree, subtree)
+    else
+        tree = subtree
+    end
+    f.close()
+}
 desc = Damage::Description::Description.new(tree)
 desc.config.damage_version = `cd #{File.dirname(__FILE__)}; git rev-parse HEAD; cd - > /dev/null`.chomp()
 
-if ARGV[1] == nil then 
+if opts[:pahole] == nil then 
     Damage::Doc::generate(desc)
     Damage::C::generate(desc)
     Damage::Ruby::generate(desc)
 else
-    input = File.open(ARGV[1])
+    input = File.open(opts[:pahole])
     desc.pahole = Damage::Description::Pahole.new(desc.config.libname, input)
     Damage::Java::generate(desc, desc.pahole)
 end
